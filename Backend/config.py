0f58543a41d63e07fa80adb9e5ad8b99cc5fa417
DarrelTran <147282675>
2025-08-22 15:48:23 -0700

    backend database connection

--- Backend/config.py
+++ Backend/config.py
@@ -1,21 +1,16 @@
-'''
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
+import pathlib
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent
 
 class UserSettings(BaseSettings):
-    databaseUsername: str = Field(env="USER_DATABASE_USERNAME")
-    databasePassword: str = Field(env="USER_DATABASE_PASSWORD")
-    databaseHost: str = Field(env="USER_DATABASE_HOST")
-    databasePort: int = Field(env="USER_DATABASE_PORT")
-    databaseName: str = Field(env="USER_DATABASE_NAME")
+    databaseUsername: str = Field(alias="USER_DATABASE_USERNAME")
+    databasePassword: str = Field(alias="USER_DATABASE_PASSWORD")
+    databaseHost: str = Field(alias="USER_DATABASE_HOST")
+    databasePort: int = Field(alias="USER_DATABASE_PORT")
+    databaseName: str = Field(alias="USER_DATABASE_NAME")
 
-    model_config = SettingsConfigDict(env_file='.env')
+    model_config = SettingsConfigDict(env_file=str(BASE_DIR / ".env"))
 
 userSettings = UserSettings()
-
-print(userSettings.databaseHost)
-'''
-
-import os
-print(os.getcwd())  # where Python thinks your working directory is
-print(open(".env").read())  # confirm Python can read it
--- Backend/db/session.py
+++ Backend/db/session.py
@@ -1,7 +1,19 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from config import userSettings
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from Backend.config import userSettings
+from asyncio import run
 
+DATABASE_NAME = f'postgresql+asyncpg://{userSettings.databaseUsername}:{userSettings.databasePassword}@{userSettings.databaseHost}:{userSettings.databasePort}/{userSettings.databaseName}'
 
+engine = create_async_engine(DATABASE_NAME)
+postgresqlSession = async_sessionmaker(autoflush=False, expire_on_commit=False, bind=engine)
 
-DATABASE_NAME = f'postgresql://{}'
+async def asyncTest():
+    try:
+        async with engine.begin() as connection:
+            await connection.execute(text("SELECT 1"))
+        print("Connection successful!")
+    except Exception as e:
+        print(f"Connection failed: {e}")
+
+run(asyncTest())

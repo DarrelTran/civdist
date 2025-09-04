9211f060fc02eb00c4c070a0c32f120159588314
DarrelTran <147282675+DarrelTran@users.noreply.github.com>
2025-09-03 21:33:21 -0700

    db link

--- Backend/db/session.py
+++ Backend/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 import os
 
-DATABASE_NAME = f'postgresql+asyncpg://postgres:{os.environ.get('USER_DATABASE_PASSWORD')}@db.uojsgkzhqrankupfuore.supabase.co:5432/postgres'
+DATABASE_NAME = os.environ.get('USER_DATABASE_STR')
 
 engine = create_async_engine(DATABASE_NAME)
 postgresqlSession = async_sessionmaker(autoflush=False, expire_on_commit=False, bind=engine)

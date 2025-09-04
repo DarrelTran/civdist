a7c084dd9d7cdefd1ecc14067cf290c65b5888ed
DarrelTran <147282675+DarrelTran@users.noreply.github.com>
2025-09-03 21:16:33 -0700

    db link fix

--- Backend/db/session.py
+++ Backend/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 import os
 
-DATABASE_NAME = f'postgresql+asyncpg://postgres.uojsgkzhqrankupfuore:[{os.environ.get('USER_DATABASE_PASSWORD')}]@aws-1-us-west-1.pooler.supabase.com:6543/postgres?pgbouncer=true'
+DATABASE_NAME = f'postgresql+asyncpg://postgres:{os.environ.get('USER_DATABASE_PASSWORD')}@db.uojsgkzhqrankupfuore.supabase.co:5432/postgres'
 
 engine = create_async_engine(DATABASE_NAME)
 postgresqlSession = async_sessionmaker(autoflush=False, expire_on_commit=False, bind=engine)
